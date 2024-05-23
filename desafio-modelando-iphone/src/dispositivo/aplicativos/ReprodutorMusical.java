2c8affe925ae33a32b1580bf20681e76c991dcfa
jhonestiago <jonestiago.94@gmail.com>
2024-05-22 22:28:38 -0300

    interface ReprodutorMusical

---
+++ desafio-modelando-iphone/src/dispositivo/aplicativos/ReprodutorMusical.java
@@ -0,0 +1,15 @@
+package dispositivo.aplicativos;
+
+// Interface da funcionalidade Reprodutor Musical
+public interface ReprodutorMusical {
+
+    // Método para tocar música
+    void tocarMusica();
+    
+    // Método para pausar a música
+    void pausarMusica();
+
+    // Método para selecionar uma música
+    void selecionarMusica(String musica);
+
+}
